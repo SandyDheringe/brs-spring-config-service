8676afe90cb7724e8a6bf3cf4a4e5ec4c418872d
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 16:35:16 +0530

    initial commit

---
+++ src/main/java/com/brsspringconfigservice/BrsSpringConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brsspringconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsSpringConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsSpringConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brsspringconfigservice/BrsSpringConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brsspringconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsSpringConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
