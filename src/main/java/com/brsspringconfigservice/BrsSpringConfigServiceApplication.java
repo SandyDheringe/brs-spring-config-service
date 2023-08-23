3205c94a17109b5d708f24065597dee9c15d2654
sandeep <sandeep.ariseup@gmail.com>
2023-08-23 15:47:23 +0530

    config server setup

--- src/main/java/com/brsspringconfigservice/BrsSpringConfigServiceApplication.java
+++ src/main/java/com/brsspringconfigservice/BrsSpringConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.brsspringconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class BrsSpringConfigServiceApplication {
 
     public static void main(String[] args) {
